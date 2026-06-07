3efdba9e4354b498fa47aa2dc75d8607589c86ce
abhirk12 <abhijeetrkulkarni12@gmail.com>
2026-06-07 17:19:34 +0530

    SCRUM-10 Design Navbar UI

---
+++ frontend/src/components/Navbar.jsx
@@ -0,0 +1,132 @@
+import React from "react";
+import { Link, useLocation, useNavigate } from "react-router-dom";
+import { useAuth } from "../context/AuthContext";
+import { FaBus } from "react-icons/fa";
+import "./Navbar.css";
+
+function Navbar() {
+  const location = useLocation();
+  const navigate = useNavigate();
+  const { isAuthenticated, logout } = useAuth();
+
+  if (
+    location.pathname === "/login" ||
+    location.pathname === "/signup"
+  ) {
+    return null;
+  }
+
+  const handleLogout = () => {
+    logout();
+    navigate("/login");
+  };
+
+  return (
+    <nav className="navbar navbar-expand-lg custom-navbar sticky-top">
+      <div className="container">
+
+        <Link className="navbar-brand" to="/">
+          <FaBus className="brand-icon" />
+          TourBooking
+        </Link>
+
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarNav"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+
+        <div
+          className="collapse navbar-collapse"
+          id="navbarNav"
+        >
+          <ul className="navbar-nav ms-auto align-items-lg-center">
+
+            {/* NEW HOME TAB */}
+            <li className="nav-item">
+              <Link className="nav-link" to="/home">
+                Home
+              </Link>
+            </li>
+
+            {isAuthenticated ? (
+              <>
+                <li className="nav-item">
+                  <Link
+                    className="nav-link"
+                    to="/profile"
+                  >
+                    Profile
+                  </Link>
+                </li>
+
+                <li className="nav-item">
+                  <Link
+                    className="nav-link"
+                    to="/mybookings"
+                  >
+                    My Bookings
+                  </Link>
+                </li>
+
+                <li className="nav-item">
+                  <Link
+                    className="nav-link"
+                    to="/schedule"
+                  >
+                    Schedule
+                  </Link>
+                </li>
+
+                <li className="nav-item">
+                  <Link
+                    className="nav-link"
+                    to="/ticket/123"
+                  >
+                    Tickets
+                  </Link>
+                </li>
+
+                <li className="nav-item ms-lg-3">
+                  <button
+                    className="btn btn-danger logout-btn"
+                    onClick={handleLogout}
+                  >
+                    Logout
+                  </button>
+                </li>
+              </>
+            ) : (
+              <>
+                <li className="nav-item">
+                  <Link
+                    className="btn btn-outline-light me-lg-2"
+                    to="/login"
+                  >
+                    Login
+                  </Link>
+                </li>
+
+                <li className="nav-item">
+                  <Link
+                    className="btn btn-warning"
+                    to="/signup"
+                  >
+                    Sign Up
+                  </Link>
+                </li>
+              </>
+            )}
+
+          </ul>
+        </div>
+
+      </div>
+    </nav>
+  );
+}
+
+export default Navbar;
