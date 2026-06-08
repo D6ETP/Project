12d3bc2778c52721cd80835f68e307d665beecdb
Tanmay Mahale <tanmaymahale199@gmail.com>
2026-06-08 18:56:45 +0530

    SCRUM-21-Create-Booking-Success-Page

---
+++ frontend/src/pages/BookingSuccessPage.jsx
@@ -0,0 +1,204 @@
+import { Link } from "react-router-dom";
+import { getBookings } from "../services/bookingService";
+
+import jsPDF from "jspdf";
+import "../pages/BookingSuccessPage.css";
+
+import { QRCodeCanvas } from "qrcode.react";
+
+function BookingSuccessPage() {
+
+  const bookings =
+    getBookings();
+
+  const booking =
+    bookings[bookings.length - 1];
+
+  if (!booking) {
+    return (
+     <div className="success-page">
+  <div className="container">
+    </div>
+        <h2>No Booking Found</h2>
+      </div>
+    );
+  }
+
+  const downloadPDF = () => {
+
+    const doc = new jsPDF();
+
+    doc.setFontSize(20);
+
+    doc.text(
+      "Bus Ticket",
+      80,
+      20
+    );
+
+    doc.setFontSize(12);
+
+    doc.text(
+      `Booking ID: ${booking.bookingId}`,
+      20,
+      50
+    );
+
+    doc.text(
+      `Passenger: ${booking.passenger}`,
+      20,
+      70
+    );
+
+    doc.text(
+      `Bus: ${booking.busName}`,
+      20,
+      90
+    );
+
+    doc.text(
+      `Route: ${booking.route}`,
+      20,
+      110
+    );
+
+    doc.text(
+      `Seats: ${booking.seats.join(", ")}`,
+      20,
+      130
+    );
+
+    doc.text(
+      `Amount: ₹${booking.amount}`,
+      20,
+      150
+    );
+
+    doc.save(
+      `${booking.bookingId}.pdf`
+    );
+  };
+
+  return (
+    <div className="container mt-5">
+
+      <div className="card shadow ticket-card">
+
+        <div className="card-body p-5">
+
+          <div className="text-center">
+
+            <h1 className="text-success">
+              🎉 Booking Successful
+            </h1>
+
+            <p>
+              Ticket Booked Successfully
+            </p>
+
+          </div>
+
+          <hr />
+
+          <div className="row">
+
+            <div className="col-md-6 booking-details">
+
+              <h5>Booking ID</h5>
+
+              <p className="booking-id">
+  {booking.bookingId}
+</p>
+
+              <h5>Passenger</h5>
+
+              <p>
+                {booking.passenger}
+              </p>
+
+              <h5>Bus Name</h5>
+
+              <p>
+                {booking.busName}
+              </p>
+
+              <h5>Route</h5>
+
+              <p>
+                {booking.route}
+              </p>
+
+              <h5>Seats</h5>
+
+              <p>
+                {booking.seats.join(", ")}
+              </p>
+
+              <h5>Amount</h5>
+
+              <p className="amount">
+                ₹{booking.amount}
+              </p>
+
+            </div>
+
+            <div className="col-md-6 qr-section">
+
+              <h5>
+                Ticket QR Code
+              </h5>
+
+              <QRCodeCanvas
+                value={booking.bookingId}
+                size={180}
+              />
+
+            </div>
+
+          </div>
+
+          <hr />
+
+          <div className="ticket-buttons">
+
+            <button
+              className="btn btn-success me-2"
+              onClick={downloadPDF}
+            >
+              Download Ticket
+            </button>
+
+            <button
+              className="btn btn-primary me-2"
+              onClick={() =>
+                window.print()
+              }
+            >
+              Print Ticket
+            </button>
+
+            <Link
+              to="/bookings"
+              className="btn btn-warning me-2"
+            >
+              My Bookings
+            </Link>
+
+            <Link
+              to="/"
+              className="btn btn-secondary"
+            >
+              Home
+            </Link>
+
+          </div>
+
+        </div>
+
+      </div>
+
+    </div>
+  );
+}
+
+export default BookingSuccessPage;
